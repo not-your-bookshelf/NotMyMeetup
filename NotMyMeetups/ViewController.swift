711a3406538d946c2412ad0220e2aeae003aafc7
Zehra Abacioglu <zehra@nyu.edu>
2018-12-03 11:13:05 -0500

    Initial Commit

---
+++ NotMyMeetups/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotMyMeetups
+//
+//  Created by Zehra Abacioglu on 12/3/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
