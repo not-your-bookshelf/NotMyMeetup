0c288c4122fd05d458b7447f7a4812fbf7e7a8e4
Zehra Abacioglu <zehra@nyu.edu>
2018-12-03 16:06:26 -0500

    not working yet

---
+++ NotMyMeetups/Meetup.swift
@@ -0,0 +1,9 @@
+//
+//  Meetup.swift
+//  NotMyMeetups
+//
+//  Created by Zehra Abacioglu on 12/3/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import Foundation
---
+++ NotMyMeetups/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  NotMyMeetups
+//
+//  Created by Zehra Abacioglu on 12/3/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
